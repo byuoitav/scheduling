f014e81318f4bf332e561dc84821dcc1ee88a4e7
Dan Clegg <dgclegg@gmail.com>
2017-10-24 08:25:12 -0600

    Watching dir

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Developer/git/byugithub/av-scheduling-o365-microservice/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
