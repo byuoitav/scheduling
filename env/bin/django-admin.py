8aac8f66de0082ca27296d43f5f0aa755a687b14
Dan Clegg <dgclegg@gmail.com>
2017-10-24 08:19:03 -0600

    Added flask and marshmallow

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Developer/git/byugithub/av-scheduling-o365-microservice/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
