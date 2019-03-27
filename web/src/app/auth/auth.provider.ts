c916d21e1951df17969487eb7440cea5a051fac4
Daniel Randall <dgrandall@me.com>
2019-03-27 11:45:54 -0600

    removing old stuff

--- web/src/app/auth/auth.provider.ts
+++ web/src/app/auth/auth.provider.ts
@@ -1,72 +1,67 @@
 /// <reference path="../../../node_modules/@types/adal/index.d.ts" />
-import { Injectable } from '@angular/core';
-import 'expose-loader?AuthenticationContext!../../../node_modules/adal-angular/lib/adal.js';
-import { AuthContext } from './authentication.context';
-import { Observable, AsyncSubject, BehaviorSubject } from 'rxjs';
-import { environment } from '../../environments/environment'
+import { Injectable } from "@angular/core";
+import "expose-loader?AuthenticationContext!../../../node_modules/adal-angular/lib/adal.js";
+import { AuthContext } from "./authentication.context";
+import { Observable, AsyncSubject, BehaviorSubject } from "rxjs";
+import { environment } from "../../environments/environment";
 let createRawAuthContext: adal.AuthenticationContextStatic = AuthenticationContext;
 declare let Logging: adal.Logging;
 
 @Injectable()
 export class AuthProvider {
+  private _context: AuthContext = null;
+  constructor() {
+    console.log("Constructing AuthProvider...");
+  }
 
-    private _context: AuthContext = null;
-    constructor() {
-        console.log('Constructing AuthProvider...');
-    }
+  getContext(): AuthContext {
+    console.log("getContext...");
 
-    getContext(): AuthContext {
-        console.log('getContext...');
+    if (this._context != null) return this._context;
 
-        if (this._context != null) return this._context;
+    let config = this.getConfig();
+    let rawContext = new createRawAuthContext(config);
+    this.enableRawLogging();
+    this._context = <AuthContext>this.extend(rawContext);
+    return this._context;
+  }
 
-        let config = this.getConfig();
-        let rawContext = new createRawAuthContext(config);
-        this.enableRawLogging();
-        this._context = <AuthContext>this.extend(rawContext);
-        return this._context;
-    }
-
-    private getConfig(): adal.Config {
-
-        /*
+  private getConfig(): adal.Config {
+    /*
         let config: adal.Config = {
-            tenant: "byu.onmicrosoft.com",
 //            clientId: environment.clientId,
             postLogoutRedirectUri: window.location.origin + '/',
             redirectUri: window.location.origin + '/'
         };
        */
-       // return config;
-       return null;
-    }
+    // return config;
+    return null;
+  }
 
-    private extend(context: adal.AuthenticationContext): any {
+  private extend(context: adal.AuthenticationContext): any {
+    (<any>context).isLoggedIn = function(): boolean {
+      return context.getCachedUser() != null;
+    };
 
-        (<any>context).isLoggedIn = function (): boolean {
-            return context.getCachedUser() != null;
-        };
+    (<any>context).processAdRedirect = function(): void {
+      console.log("process ad redirect...");
+      context.verbose("Processing the hash: " + location.hash);
+      if (context.isCallback(location.hash)) {
+        let requestInfo = context.getRequestInfo(location.hash);
+        context.saveTokenFromHash(requestInfo);
+      }
+    };
 
-        (<any>context).processAdRedirect = function (): void {
-            console.log('process ad redirect...');
-            context.verbose('Processing the hash: ' + location.hash);
-            if (context.isCallback(location.hash)) {
-                let requestInfo = context.getRequestInfo(location.hash);
-                context.saveTokenFromHash(requestInfo);
-            }
-        };
+    return context;
+  }
 
-        return context;
-    }
+  private enableRawLogging(): void {
+    Logging.level = 3;
+    Logging.log = this.log;
+  }
 
-    private enableRawLogging(): void {
-        Logging.level = 3;
-        Logging.log = this.log;
-    }
-
-    private log(message: string): void {
-        console.log('adal logging...');
-        console.log(message);
-    }
-
+  private log(message: string): void {
+    console.log("adal logging...");
+    console.log(message);
+  }
 }
